6e26d5586cb6f01a2dd1c5a7f3c9af8d4e7ecd6f
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-01-05 19:15:17 -0300

    versão 1.2
    com problemas

--- calculadora/src/app/calculadora/calculadora.module.ts
+++ calculadora/src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './componentes';
+import { CalculadoraService } from './services';
+
 
 
 
 @NgModule({
   imports: [ CommonModule ],
   declarations: [ CalculadoraComponent ],
-  exports: [ CalculadoraComponent ]
+  exports: [ CalculadoraComponent ],
+  providers: [ CalculadoraService ]
 })
 export class CalculadoraModule { }
---
+++ calculadora/src/app/calculadora/services/index.ts
@@ -0,0 +1,3 @@
+export * from './calculadora.service';
+export * from './componentes';
+export * from './services';
