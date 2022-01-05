d3fefb33a03e5afae553ab22c6f65e910aaf9756
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-01-05 18:09:14 -0300

    versão 1.1

--- calculadora/src/app/calculadora/calculadora.module.ts
+++ calculadora/src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './componentes';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ CommonModule ],
+  declarations: [ CalculadoraComponent ],
+  exports: [ CalculadoraComponent ]
 })
 export class CalculadoraModule { }
