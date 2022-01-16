841117d637aed9a6a8935dedf4ad1b398a624b67
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-01-16 17:02:14 -0300

    alguns erros

--- tour-of-heroes/src/app/app-routing.module.ts
+++ tour-of-heroes/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'Full' },
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'heroes/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent },
 ];
 
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,20 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit{
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
@@ -26,9 +23,4 @@ export class HeroesComponent implements OnInit{
       (heroes) => (this.heroes = heroes));
   }
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(
-      `HeroesComponent: Selected hero id=${hero.id}`);
-  }
 }
